d6c1ebfc065083a933cd91b37edad9f8a89103dd
wizzt <wizztver@gmail.com>
2024-10-28 00:30:30 +0300

    refactoring

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,100 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+from src.features.build_features import *
+from src.constants import (
+    QUANTILE_25_VALUE,
+    QUANTILE_75_VALUE,
+    AUTOCORRELATION_LAG,
+    FFT_COEFFICIENT_0,
+    FFT_COEFFICIENT_1,
+    NUMBER_CROSSING_VALUE
+)
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def remove_rows_with_null_values(df: pd.DataFrame) -> pd.DataFrame:
+    return df[~df['values'].apply(lambda x: any(pd.isnull(v) for v in x))]
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def generate_features(df: pd.DataFrame) -> pd.DataFrame:
+    feature_dict = {
+        'id': df['id'],
+        'start_date': df['dates'].apply(get_start_date),
+        'end_date': df['dates'].apply(get_end_date),
+        'duration': df['dates'].apply(calculate_duration)
+    }
+    
+    for name, func in globals().items():
+        if hasattr(func, 'is_feature'):
+            if name == 'quantile':
+                feature_dict['quantile_25'] = df['values'].apply(
+                    lambda values: func(values, QUANTILE_25_VALUE)
+                )
+                feature_dict['quantile_75'] = df['values'].apply(
+                    lambda values: func(values, QUANTILE_75_VALUE)
+                )
+            elif name == 'autocorrelation':
+                feature_dict['autocorrelation_lag_1'] = df['values'].apply(
+                    lambda values: func(values, AUTOCORRELATION_LAG)
+                )
+            elif name == 'fft_coefficient':
+                feature_dict['fft_coefficient_0'] = df['values'].apply(
+                    lambda values: np.abs(func(values, FFT_COEFFICIENT_0))
+                )
+                feature_dict['fft_coefficient_1'] = df['values'].apply(
+                    lambda values: np.abs(func(values, FFT_COEFFICIENT_1))
+                )
+            elif name == 'number_crossing_m':
+                feature_dict['number_crossing_0'] = df['values'].apply(
+                    lambda values: func(values, NUMBER_CROSSING_VALUE)
+                )
+            else:
+                feature_dict[name] = df['values'].apply(func)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    processed_df = pd.DataFrame(feature_dict)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    if 'label' in df.columns:
+        processed_df['target'] = df['label']
 
-    main()
+    return processed_df
+
+
+def reduce_mem_usage(df: pd.DataFrame) -> pd.DataFrame:
+    start_mem = df.memory_usage().sum() / 1024**2
+    print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype.name
+
+        if col_type not in ["object", "category", "datetime64[ns, UTC]"]:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
+    print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
+
+
+def make_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    return reduce_mem_usage(
+        generate_features(
+            remove_rows_with_null_values(df)
+        )
+    )

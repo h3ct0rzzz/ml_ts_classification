fc574338f52194092ba704031a5c5c83f83786b2
h3ct0rzzz <hectorztver@gmail.com>
2024-10-28 16:01:20 +0300

    temp del func reduce_mem_usage()

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -73,43 +73,43 @@ def generate_features(df: pd.DataFrame) -> pd.DataFrame:
     return processed_df
 
 
-def reduce_mem_usage(df: pd.DataFrame) -> pd.DataFrame:
-    start_mem = df.memory_usage().sum() / 1024**2
-    print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
+# def reduce_mem_usage(df: pd.DataFrame) -> pd.DataFrame:
+#     start_mem = df.memory_usage().sum() / 1024**2
+#     print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
 
-    for col in df.columns:
-        col_type = df[col].dtype.name
+#     for col in df.columns:
+#         col_type = df[col].dtype.name
 
-        if col_type not in ["object", "category", "datetime64[ns, UTC]"]:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == "int":
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
+#         if col_type not in ["object", "category", "datetime64[ns, UTC]"]:
+#             c_min = df[col].min()
+#             c_max = df[col].max()
+#             if str(col_type)[:3] == "int":
+#                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+#                     df[col] = df[col].astype(np.int8)
+#                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+#                     df[col] = df[col].astype(np.int16)
+#                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+#                     df[col] = df[col].astype(np.int32)
+#                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+#                     df[col] = df[col].astype(np.int64)
+#             else:
+#                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+#                     df[col] = df[col].astype(np.float16)
+#                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+#                     df[col] = df[col].astype(np.float32)
+#                 else:
+#                     df[col] = df[col].astype(np.float64)
 
-    end_mem = df.memory_usage().sum() / 1024**2
-    print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
-    print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
+#     end_mem = df.memory_usage().sum() / 1024**2
+#     print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
+#     print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
 
-    return df
+#     return df
 
 
 def make_dataset(df: pd.DataFrame) -> pd.DataFrame:
     df = fill_missing_values(df)
     df = generate_features(df)
-    df = reduce_mem_usage(df)
+    # df = reduce_mem_usage(df)
     df = df.fillna(df.mean())
     return df

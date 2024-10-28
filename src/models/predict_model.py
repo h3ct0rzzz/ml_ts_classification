25fc7fef9dc427e467a06b57f998ec0f95007bb1
h3ct0rzzz <hectorztver@gmail.com>
2024-10-28 15:23:22 +0300

    add prev inference code

---
+++ src/models/predict_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from catboost import CatBoostRegressor
+
+
+def load_model(path: str) -> CatBoostRegressor:
+    pass
+
+
+def predict(model: CatBoostRegressor, df: pd.DataFrame) -> np.ndarray:
+    pass
+

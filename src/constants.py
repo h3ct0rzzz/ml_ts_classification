065f19709d94fd5be852ccb514f2fd6505eb58de
wizzt <wizztver@gmail.com>
2024-10-29 22:27:30 +0300

    update: add hyperparams catboost

--- src/constants.py
+++ src/constants.py
@@ -31,13 +31,33 @@ ENERGY_RATIO_DEFAULT_SEGMENT_FOCUS = 0
 
 # CatBoost best hyperparameters
 BEST_PARAMS = {
-    'depth': None,
-    'learning_rate': None,
-    'l2_leaf_reg': None,
-    'random_strength': None,
-    'bagging_temperature': None,
-    'od_type': None,
-    'od_wait': None
+    'learning_rate': 0.05549981700358849,
+    'l2_leaf_reg': 17,
+    'colsample_bylevel': 0.7096296254119971,
+    'auto_class_weights': 'Balanced',
+    'depth': 7,
+    'bootstrap_type': 'MVS',
+    'boosting_type': 'Plain',
+    'random_strength': 2.7716379366222275,
+    'iterations': 1000,
+    'loss_function': 'Logloss'
 }
 
-MODEL_PATH = 'models/'
+# List of features
+LIST_OF_FEATURES = [
+    'sum_values', 'linear_trend', 'mean', 'last_location_of_minimum',
+    'autocorrelation_lag_1', 'mean_abs_change',
+    'percentage_of_reoccurring_datapoints',
+    'percentage_of_reoccurring_values', 'fft_coefficient_1',
+    'last_location_of_maximum', 'ratio_unique_values', 'abs_energy',
+    'sum_of_reoccurring_values', 'quantile_75', 'mean_change',
+    'phase_duration', 'variation_coefficient', 'time_series_entropy',
+    'root_mean_square', 'duration', 'first_location_of_maximum',
+    'first_location_of_minimum', 'absolute_sum_of_changes',
+    'fft_coefficient_0', 'skewness', 'end_date', 'variance',
+    'mean_second_derivative_central', 'standard_deviation',
+    'start_date', 'maximum', 'median', 'number_crossing_0',
+    'benford_correlation', 'permutation_entropy',
+    'energy_ratio_by_chunks', 'kurtosis', 'quantile_25',
+    'count_above_mean'
+]
